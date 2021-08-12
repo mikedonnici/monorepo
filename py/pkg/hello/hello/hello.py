49f207ed4f9da2863453e6b1a45fee2872c50a9d
Mike Donnici <michael.donnici@dpie.nsw.gov.au>
2021-08-12 14:09:36 +1000

    Update hello package.

--- py/pkg/hello/hello/hello.py
+++ py/pkg/hello/hello/hello.py
@@ -3,4 +3,4 @@ from faker import Faker
 
 def say_hi():
     fake = Faker()
-    print(f"Hi {fake.name()}, from the all new and improved hello package.")
+    print(f"Hi {fake.name()}, from hello v0.1.1.")
--- py/pkg/hello/tests/test_hello.py
+++ py/pkg/hello/tests/test_hello.py
@@ -1,5 +1,10 @@
+import hello.hello
 from hello import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.1'
+
+
+def test_say_hi():
+    hello.hello.say_hi()
